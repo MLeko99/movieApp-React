92e10787c90a14ec2893179f1c6a591984c9f5a2
Mile Leko <mile.leko@feal.local>
2025-01-08 15:21:47 +0100

    Final final

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "../styles/Loader.css";
 
-const Loader = () => <div className="loader"></div>;
+const Loader = () => (
+  <div className="loader">
+    <p>Učitavanje...</p>
+  </div>
+);
 
 export default Loader;
