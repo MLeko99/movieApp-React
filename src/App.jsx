4b41c64734e546b0bf6c4fc0451e561d50de6939
MLeko99 <mile.leko80@gmail.com>
2025-01-19 18:53:46 +0100

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import "./index.css";
 
-function App() {}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/tv" />} />
+        <Route path="/movies" element={<Home activeTab="movies" />} />
+        <Route path="/tv" element={<Home activeTab="tv" />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
