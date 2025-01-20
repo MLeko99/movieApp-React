9bb035b0c6f2b95c17bfc79f41113c1f18bbd43f
MLeko99 <mile.leko80@gmail.com>
2025-01-20 20:39:21 +0100

    Main component updated with all the necessary routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+import TVShowDetail from "./pages/TVShowDetail";
 import Header from "./components/Header";
 import "./index.css";
 
@@ -19,6 +21,8 @@ const App = () => (
         <Route path="/" element={<Navigate to="/tv" />} />
         <Route path="/movies" element={<Home activeTab="movies" />} />
         <Route path="/tv" element={<Home activeTab="tv" />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<TVShowDetail />} />
       </Routes>
     </Router>
   </Provider>
